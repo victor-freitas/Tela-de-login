5d13a42b6e7ec2742c9c5196c10615882814b479
victor-freitas <vitorm3rcy@gmail.com>
2020-03-07 01:42:14 -0300

    Deploy v.0.0.1

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL, MySQLdb
+from flask_cors import CORS
+import bcrypt
+
+
+app = Flask(__name__)
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+app.secret_key = "^A%DJAJU^JJ123"
+
+
+app.config['MYSQL_HOST'] = 'sql10.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql10325576'
+app.config['MYSQL_PASSWORD'] = 'Dqm26RMtFv'
+app.config['MYSQL_DB'] = 'sql10325576'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM users WHERE email=%s",(email,))
+        user = cur.fetchone()
+        cur.close()
+
+        if len(user) > 0:
+            if bcrypt.hashpw(password, user["password"].encode('utf-8')) == user["password"].encode('utf-8'):
+                session['name'] = user['name']
+                session['email'] = user['email']
+                return render_template("home.html")
+            else:
+                return "Error, senha e email errados !"
+        else:
+            return "Error, usuário não encontrado"
+    else:
+        return render_template("login.html")
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template("home.html")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (name, email, password) VALUES (%s,%s,%s)", (name,email,hash_password,))
+        mysql.connection.commit()
+        session['name'] = request.form['name']
+        session['email'] = request.form['email']
+        return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
